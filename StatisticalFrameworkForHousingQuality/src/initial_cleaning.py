10c20b35e4a4c1131a6efe77e81b60fb808bb2b3
Damien Chambon <damien_chambon@outlook.com>
2021-03-27 18:47:51 +0100

    Styled the code and fixed some bugs

--- StatisticalFrameworkForHousingQuality/src/initial_cleaning.py
+++ StatisticalFrameworkForHousingQuality/src/initial_cleaning.py
@@ -5,51 +5,55 @@
 import pandas as pd
 import numpy as np
 from sklearn.decomposition import PCA
-import matplotlib.pyplot as plt
-import seaborn as sns; sns.set()
 
 # We read the dataset as a Pandas DataFrame (easier for analysis)
-df = pd.read_csv('dataset_raw.csv')
-df = df.drop(columns=['index'])
+df = pd.read_csv('data/dataset_raw.csv', index_col=0)
+df.reset_index(inplace=True, drop=True)
 
 # we work with the walls variables
-walls = df[["_d3","_d4"]]
+walls = df[["_d3", "_d4"]]
 # we create a column for severity_wall
 col = pd.DataFrame({'severity_walls': []})
 walls = walls.join(col)
-walls = walls.drop(walls[walls["_d3"] == 8 ].index) # "8" represents the absence of response
-walls = walls.replace(9,0)
+walls = walls.drop(walls[walls["_d3"] == 8].index)
+# "8" represents the absence of response
+walls = walls.replace(9, 0)
 walls['severity_walls'] = walls['_d3'] + walls['_d4']
 
 # we work with the windows variables
-windows = df[["_e1","_e2","_e3"]]
+windows = df[["_e1", "_e2", "_e3"]]
 # we create a column for severity_windows
 col = pd.DataFrame({'severity_windows': []})
 windows = windows.join(col)
-windows = windows.drop(windows[windows["_e1"] == 8 ].index) # "8" represents the absence of response
-windows = windows.replace(9,0)
+windows = windows.drop(windows[windows["_e1"] == 8].index)
+# "8" represents the absence of response
+windows = windows.replace(9, 0)
 windows['severity_windows'] = windows['_e1'] + windows['_e2'] + windows['_e3']
 
 # we work with the stairways variables
-stairways = df[["_f1","_f2"]]
+stairways = df[["_f1", "_f2"]]
 # we create a column for severity_stairways
 col = pd.DataFrame({'severity_stairways': []})
 stairways = stairways.join(col)
-stairways = stairways.drop(stairways[stairways["_f1"] == 8 ].index) # "8" represents the absence of response
-stairways = stairways.replace(9,0)
+stairways = stairways.drop(stairways[stairways["_f1"] == 8].index)
+# "8" represents the absence of response
+stairways = stairways.replace(9, 0)
 stairways['severity_stairways'] = stairways['_f1'] + stairways['_f2']
 
 # we work with the floors variables
-floors = df[["_g3","_g4",]]
+floors = df[["_g3", "_g4"]]
 # we create a column for severity_floors
 col = pd.DataFrame({'severity_floors': []})
 floors = floors.join(col)
-floors = floors.drop(floors[floors["_g3"] == 8 ].index) # "8" represents the absence of response
-floors = floors.replace(9,0)
+floors = floors.drop(floors[floors["_g3"] == 8].index)
+# "8" represents the absence of response
+floors = floors.replace(9, 0)
 floors['severity_floors'] = floors['_g3'] + floors['_g4']
 
 # concatenate the severity for the features in a new Dataframe
-severity = pd.concat([walls["severity_walls"],windows["severity_windows"],stairways['severity_stairways'],floors['severity_floors']], axis=1)
+severity = pd.concat([walls["severity_walls"], windows["severity_windows"],
+                      stairways['severity_stairways'],
+                      floors['severity_floors']], axis=1)
 severity = severity.dropna()
 severity = severity.apply(np.int64)
 
@@ -76,11 +80,14 @@ df_cond_pca = pca.transform(df_cond)
 df_cond["pca"] = df_cond_pca
 
 # we scale the obtained pca scores between 0 and 10
-df_cond["pca_scaled"] = np.interp(df_cond["pca"], (df_cond["pca"].min(), df_cond["pca"].max()), (0, 10))
+df_cond["pca_scaled"] = np.interp(df_cond["pca"],
+                                  (df_cond["pca"].min(),
+                                   df_cond["pca"].max()), (0, 10))
 
 
-# we create a Pandas Dataframe that contains the predictors used in the analysis
-df_pred= pd.DataFrame()
+# we create a Pandas Dataframe that contains the predictors
+# used in the analysis
+df_pred = pd.DataFrame()
 df_pred["Householder sex"] = df["_1b"]
 df_pred["Householder age"] = df["_1c"]
 df_pred["Householder hispanic origin"] = df["_1e"]
@@ -112,10 +119,10 @@ for elem in df_pred["Length of lease"]:
 df_pred["Status"] = list_statuses
 df_pred
 
-# we create a Pandas Dataframe that contains the predictors used in the analysis, 
-# the housing conditions and the PCA scores
+# we create a Pandas Dataframe that contains the predictors
+# used in the analysis, the housing conditions and the PCA scores
 final_df = pd.DataFrame()
 final_df = df_pred.join(df_cond)
 
 # exporting of the dataset
-final_df.to_csv('dataset_processed.csv', index=False)
+final_df.to_csv('data/dataset_processed.csv', index=False)
